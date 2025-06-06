7a61a578a39026dabc83185354ca09490eb71f73
Tom de Lima <antoniojosedelimaneto@gmail.com>
2025-06-05 22:51:48 -0300

    Som

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Fade-in suave da página
 document.addEventListener("DOMContentLoaded", () => {
   document.body.style.opacity = 0
   setTimeout(() => {
@@ -5,3 +6,19 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.style.opacity = 1
   }, 100)
 })
+
+// Função que será disparada pelo botão
+const musica = new Audio("Music.MP3") // Verifique se o arquivo está nomeado exatamente assim
+musica.loop = true
+musica.volume = 0.15
+
+function tocarMusica() {
+  musica
+    .play()
+    .then(() => {
+      document.querySelector(".romantic-play").style.display = "none" // esconde botão após o clique
+    })
+    .catch((err) => {
+      console.log("Erro ao iniciar música:", err.message)
+    })
+}
